6c5e724f84d93bbf1ef96eef43dc34f4b10ae29b
Bao Dinh <bao.n.dinh@gmail.com>
2026-04-09 23:52:37 -0400

    init files for query and llm adapter modules

---
+++ datasheetai/llm_adapter/adapter.py
@@ -0,0 +1,6 @@
+# datasheetai/llm_adapter/adapter.py
+
+import logging
+import pandas as pd
+
+logging = logging.getLogger(__name__)
---
+++ datasheetai/query_service/query_service.py
@@ -0,0 +1,6 @@
+# datasheetai/query_service/query_service.py
+
+import logging
+import pandas as pd
+
+logging = logging.getLogger(__name__)
