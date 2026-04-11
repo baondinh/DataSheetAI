f49421c41111ae5b32f76758b2d07ced34d42ac1
Bao Dinh <bao.n.dinh@gmail.com>
2026-04-10 23:58:47 -0400

    update plan for adapter and validator pathway

--- datasheetai/llm_adapter/adapter.py
+++ datasheetai/llm_adapter/adapter.py
@@ -4,3 +4,13 @@ import logging
 import pandas as pd
 
 logging = logging.getLogger(__name__)
+
+'''
+TODO: Implement LLMAdapter class 
+- Will take str natural language query 
+- Will connect to LLM API and use env vars for authentication (secret management to be implemented later)
+- Will send prompt to LLM and receive SQL response
+- Needs to send response to validator to ensure SQL is safe (hardcode banned keywords for now)
+- Will interact with DatabaseManager to execute queries and return results
+- IMPORTANT: Do not allow direct execution of arbitrary SQL queries
+'''
